2bdc13bc40d328e5dbf0b7e81c57049d0bfcdf75
91juhwang <91juhwang@gmail.com>
2016-09-14 14:17:18 -0400

    slide navigation added and control arrow deleted

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   $('#fullpage').fullpage({
   	 anchors: ['homee', 'projectss', 'aboutt', 'contactss'],
   	 menu: '#nav_active_menus',
+  	 controlArrows: false,
   	 showActiveTooltip: false,
   	 slidesNavigation: true,
   	 slidesNavPosition: 'bottom'
