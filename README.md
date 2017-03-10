cd18c934eb480e36bc2582096dcbad28bbc6369a
James Hwang <91juhwang@gmail.com>
2017-03-10 15:27:27 -0500

    Update experience section

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# [Portfolio] www.jameshwang.me
+
+## Projects listed:
+* www.happythought.com
+* 91juhwang.github.io/JukeBox-
+* 91juhwang.github.io/digitalClock/clock.html
+
---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  $('#fullpage').fullpage({
+  	 anchors: ['homee','experiencess', 'projectss', 'aboutt', 'contactss'],
+  	 menu: '#nav_active_menus',
+  	 controlArrows: false,
+  	 showActiveTooltip: false,
+  	 slidesNavigation: true,
+  	 slidesNavPosition: 'bottom'
+ 	});
+});
+
